9f13caf6907039dc1e1bb057384005f827052e95
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-05-24 15:56:08 +0300

    adding data

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { ParseMutableClientConfiguration in
+            //to connect server
+            ParseMutableClientConfiguration.applicationId = "X9w6KrDzDBt57XoGMCb2ulygHC68M7ni42UZoXuF"
+            ParseMutableClientConfiguration.clientKey = "HUZrkSfUJO9vryzARJf9yOSFDXenHNzDBR2UV0YA"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+            
+        }
+        Parse.initialize(with: configuration)
+        
+        
         return true
     }
 
--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Apple"
+        parseObject["calories"] = 100
+        parseObject.saveInBackground{(success, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+            }else{
+                print("uploaded")
+            }
+            
+        }
+        
+        
     }
 
 

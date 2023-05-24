0bf6b1d62177f62fd415c2a253f8b112cdd04fca
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-05-24 16:06:25 +0300

    adding data

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
                 print(error?.localizedDescription)
             }else{
                 print("uploaded")
+                
             }
             
         }
